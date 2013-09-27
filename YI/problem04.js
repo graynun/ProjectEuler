79698aaf84d167ff2ef1ceedb4d48365caf0b892
graynun <graynun@gmail.com>
2013-09-27 09:43:27 +0900

    이게뭐여

--- YI/problem04.js
+++ YI/problem04.js
@@ -4,34 +4,34 @@ var arrPa = [];
 
 
 var makeMultiply = function() {
-	for(var i=100;i<1000;i++){
-		for(var j=100;j<1000;j++){
-			arr.push(i*j);
-		}
-	}
+  for(var i=100;i<1000;i++){
+    for(var j=100;j<1000;j++){
+      arr.push(i*j);
+    }
+  }
 
 };
 
 
 var findIfPalin = function(array) {
-	this.arr = array;
-	for(i=0;i<arr.length;i++){
-		var str = arr[i].toString();
+  this.arr = array;
+  for(i=0;i<arr.length;i++){
+    var str = arr[i].toString();
 
-		if(str.length == 5){
-			var front = str.slice(0,2);
-			var end = reverse(str.slice(3));
-			if(front == end){
-				arrPa.push(arr[i]);
-			}
-		}else if(str.length==6){
-			var front = str.slice(0,3);
-			var end = reverse(str.slice(3));
-			if(front == end){
-				arrPa.push(arr[i]);
-			}
-		}
-	}
+    if(str.length == 5){
+      var front = str.slice(0,2);
+      var end = reverse(str.slice(3));
+      if(front == end){
+        arrPa.push(arr[i]);
+      }
+    }else if(str.length==6){
+      var front = str.slice(0,3);
+      var end = reverse(str.slice(3));
+      if(front == end){
+        arrPa.push(arr[i]);
+      }
+    }
+  }
 };
 
 
@@ -41,8 +41,8 @@ var reverse = function(str){
 
 
 var sortArr = function(arr){
-	this.arr = arr;
-	arr.sort(function(a,b){return a-b});
+  this.arr = arr;
+  arr.sort(function(a,b){return a-b});
 
 };
 
